0b60890f83b7067932964a54c2518b4aa9bf6232
Mohammad Javad Naderi <mjnaderi@gmail.com>
2013-08-27 21:51:24 +0430

    minor change :)

--- application/controllers/Install.php
+++ application/controllers/Install.php
@@ -178,7 +178,7 @@ class Install extends CI_Controller {
 				('enable_registration', '0'),
 				('mail_from', 'shj@sharifjudge.ir'),
 				('mail_from_name', 'Sharif Judge'),
-				('reset_password_mail', '<p>\nSomeone requested to reset the password for account with this email address at {SITE_URL}.\n</p>\n<p>\nTo change your password, visit this link:\n</p>\n<p>\n<a href=\"{RESET_LINK}\">Reset Password</a>\n</p>\n<p>\nIf you don''t want to change your password, just ignore this email.\n</p>'),
+				('reset_password_mail', '<p>\nSomeone requested to reset the password for account with this email address at {SITE_URL}.\n</p>\n<p>\nTo change your password, visit this link:\n</p>\n<p>\n<a href=\"{RESET_LINK}\">Reset Password</a>\n</p>\n<p>\nThe link is valid for {VALID_TIME}. If you don''t want to change your password, just ignore this email.\n</p>'),
 				('add_user_mail', '<p>\nHello! You are registered in Sharif Judge at {SITE_URL} as {ROLE}.\n</p>\n<p>\nYour username: {USERNAME}\n</p>\n<p>\nYour password: {PASSWORD}\n</p>\n<p>\nYou can log in at <a href=\"{LOGIN_URL}\">{LOGIN_URL}</a>\n</p>'),
 				('moss_userid', ''),
 				('results_per_page', '40'),
