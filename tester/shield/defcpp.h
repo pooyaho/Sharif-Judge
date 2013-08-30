d210762890cdc98a14c1bea3f765d2746d585865
Mohammad Javad Naderi <mjnaderi@gmail.com>
2013-08-30 23:19:17 +0430

    corrected mistake

--- tester/shield/defcpp.h
+++ tester/shield/defcpp.h
@@ -8,5 +8,4 @@ Lots of "define"s that work in C, don't work correctly in C++.
 
 */
 
-#define system errorNo1         //"system" is not allowed
-#define goto errorNo2           //"goto" is not allowed
+#define goto errorNo1           //"goto" is not allowed
